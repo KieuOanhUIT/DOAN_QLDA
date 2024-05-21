9d231934262abf98bfc7099b8454cae8be573563
KieuOanhUIT <22521062@gm.uit.edu.vn>
2024-05-21 10:21:44 +0700

    connect db

---
+++ database/database.php
@@ -0,0 +1,21 @@
+<?php
+include '/DOAN_WEBSITE/database/config.php';
+class Database{
+    public $servername = servername;
+    public $username = username;
+    public $password = password;
+    public $databaseName = databaseName;
+    public $conn;
+
+    public function __construct(){
+        this->connect();
+    }
+
+    public function connect(){
+        $this->conn = new mysqli($this->servername, $this->username, $this->password, $this->databaseName);
+        if ($this->conn->connect_error) {
+            die("Connection failed: ". $this->conn->connect_error);
+        }
+        echo "Connected successfully </br>";
+    }
+}
