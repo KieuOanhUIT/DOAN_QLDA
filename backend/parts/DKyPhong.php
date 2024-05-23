abf95831943e285a256baf4a288ae6d26678bad5
KieuOanhUIT <22521062@gm.uit.edu.vn>
2024-05-23 17:35:02 +0700

    update DKYPhong

--- backend/parts/DKyPhong.php
+++ backend/parts/DKyPhong.php
@@ -1,14 +1,27 @@
 <?php
 include 'header.php';
-include 'C:\xampp\htdocs\DOAN_WEBSITE\database\database.php';
+include 'C:\xampp\htdocs\DOAN_WEBSITE\backend\PhongClass.php';
+$phongClass = new PhongClass();
+?>
+<?php
+  $selectAll=$phongClass->selectAll();
+
+   if($_SERVER['REQUEST_METHOD']=='POST'){
+    $ngay = new PhongCLass;
+    $ngayy = $_POST['ngay'];
+    $getLich = $ngay->selectByNgay($ngayy);
+    if($getLich){
+      $resultID = $getLich->fetch_assoc();
+    }
+   }
+
 ?>
 
 <body>
+<form action="" method="post">
   <div class="label">Thời gian</div>
   <div class="row-nhaptg">
-    <input class="search" type="text" placeholder="Nhập ngày" />
-    <input class="search" type="text" placeholder="Nhập tháng" />
-    <input class="search" type="text" placeholder="Nhập năm" />
+    <input class="search" type="text" name="ngay" placeholder="Nhập ngày/tháng/năm" />
     <button class="search-button">Xem phòng trống</button>
   </div>
   <div class="label">Đăng ký phòng</div>
@@ -18,6 +31,47 @@ include 'C:\xampp\htdocs\DOAN_WEBSITE\database\database.php';
     <input class="search" type="text" placeholder="Nhập mã phòng cần mượn" />
     <button class="search-button">Đăng ký</button>
   </div>
+  <table>
+    <tr>
+      <th>Mã phòng</th>
+      <th>Tên phòng</th>
+      <th>Thời gian mượn</th>
+      <th>Ngày</th>
+      <th>Tình trạng</th>
+    </tr>
+      <tr>
+        <td><?php echo $resultID['MAP'] ?></td>
+        <td><?php echo $resultID['TENP'] ?></td>
+        <td><?php echo $resultID['TGMUON'] ?></td>
+        <td><?php echo $resultID['NGAY'] ?></td>
+        <td><?php echo $resultID['TINHTRANG'] ?></td>
+      </tr>
+  </table>
+
+  <table>
+    <tr>
+      <th>Mã phòng</th>
+      <th>Tên phòng</th>
+      <th>Thời gian mượn</th>
+      <th>Ngày</th>
+      <th>Tình trạng</th>
+    </tr>
+    <?php
+      if($selectAll->num_rows >0){ while($result = $selectAll->fetch_assoc()){
+    ?>
+      <tr>
+        <td><?php echo $result['MAP'] ?></td>
+        <td><?php echo $result['TENP'] ?></td>
+        <td><?php echo $result['TGMUON'] ?></td>
+        <td><?php echo $result['NGAY'] ?></td>
+        <td><?php echo $result['TINHTRANG'] ?></td>
+      </tr>
+    <?php
+      }}
+    ?>
+  </table>
+</form>
+  
   
   <!-- them bang -->
   <table>
