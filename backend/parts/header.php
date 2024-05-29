dcbe4d661333322ef5e06d281443684ac4423374
KieuOanhUIT <22521062@gm.uit.edu.vn>
2024-05-29 07:41:45 +0700

    update link

--- backend/parts/header.php
+++ backend/parts/header.php
@@ -5,13 +5,13 @@
   <meta charset="UTF-8" />
   <title>Công ty TUFO</title>
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <link rel="stylesheet" type="text/css" href="/DOAN_WEBSITE/pages/css/reset.min.css" />
+  <link rel="stylesheet" type="text/css" href="/DOAN_QLDA/pages/css/reset.min.css" />
   <link rel="stylesheet" type="text/css" href="/DOAN_QLDA/pages/css/header.css" />
   <link rel="stylesheet" type="text/css" href="/DOAN_QLDA/pages/css/footer.css" />
-  <link rel="stylesheet" type="text/css" href="/DOAN_WEBSITE/pages/css/ChinhSuaNV.css" />  
-  <link rel="stylesheet" type="text/css" href="/DOAN_WEBSITE/pages/css/QLNV.css" />  
-  <link rel="stylesheet" type="text/css" href="/DOAN_WEBSITE/pages/css/DKyPhong.css" />  
-  <link rel="stylesheet" type="text/css" href="/DOAN_WEBSITE/pages/css/LichPhong.css" />  
+  <link rel="stylesheet" type="text/css" href="/DOAN_QLDA/pages/css/ChinhSuaNV.css" />  
+  <link rel="stylesheet" type="text/css" href="/DOAN_QLDA/pages/css/QLNV.css" />  
+  <link rel="stylesheet" type="text/css" href="/DOAN_QLDA/pages/css/DKyPhong.css" />  
+  <link rel="stylesheet" type="text/css" href="/DOAN_QLDA/pages/css/LichPhong.css" />  
   <link rel="stylesheet" type="text/css" href="/DOAN_QLDA/pages/css/SendRequest.css" />
   
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -31,9 +31,9 @@
           <li class="nav__item"><a href="#">Trang chủ</a></li>
           <li class="nav__item"><a href="#">Quy định</a></li>
           <li class="nav__item"><a href="#">Xử lý yêu cầu</a></li>
-          <li class="nav__item"><a href="/DOAN_WEBSITE/backend/parts/LichPhong.php">Lịch phòng</a></li>
-          <li class="nav__item"><a href="/DOAN_WEBSITE/backend/parts/DKyPhong.php">Đăng ký phòng</a></li>
-          <li class="nav__item"><a href="/DOAN_WEBSITE/backend/parts/QLNV.php">Quản lý nhân viên</a></li>
+          <li class="nav__item"><a href="/DOAN_QLDA/backend/parts/LichPhong.php">Lịch phòng</a></li>
+          <li class="nav__item"><a href="/DOAN_QLDA/backend/parts/DKyPhong.php">Đăng ký phòng</a></li>
+          <li class="nav__item"><a href="/DOAN_QLDA/backend/parts/QLNV.php">Quản lý nhân viên</a></li>
           <li class="nav__item"><a href="#">Nhân viên</a></li>
         </ul>
       </nav>
